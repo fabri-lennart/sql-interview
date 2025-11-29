2c48b450fccf3cfe81ab1078ed4164ab457b3465
Fabricio <fabricioflores515@gmail.com>
2025-11-29 18:42:09 -0300

    Update 01-basic.sql

--- Queries/01-basic.sql
+++ Queries/01-basic.sql
@@ -8,6 +8,40 @@ SELECT Region, sum(Sales)
 FROM transactions
 GROUP BY Region;
 
-/* CALCULATE THE TOTAL SALES */`
+/* CALCULATE THE TOTAL SALES */
 SELECT sum(Sales)
 FROM transactions
+
+/* SEARCH FOR QUANTITY OF SALES OF A ESPECIFIC PRODUCT
+IN THE STOCK (Avery 509) */
+SELECT
+  "Product Name",
+  SUM(Quantity) AS total_quantity
+FROM transactions
+WHERE "Product Name" LIKE 'Avery 509%'
+GROUP BY "Product Name";
+
+/* ORDER THE CATEGORIES BY THE SALES IN DESCENDING ORDER (TOP TO DOWN)  */
+
+SELECT Category, sum(Sales)
+  FROM transactions
+GROUP BY Category
+ORDER BY sum(Sales) DESC;
+
+/* FIND ALL PRODUCTS WHERE THE PRODUCT NAME STARTS WITH "Staples". */
+SELECT "Product Name" FROM transactions
+WHERE "Product Name" LIKE 'Staples%';
+
+/* DISPLAY THE MAXIMUM SALES VALUE AND MINIMUM SALES VALUE. */
+/* SALES BY YEAR OF ORDER DATE */
+
+SELECT YEAR("Order Date") AS year, sum(Sales)
+FROM transactions
+GROUP BY year;
+
+/* SHOW ALL THE PRODUCTS WHICH WERE SENT BY SECOND CLASS */
+
+SELECT "Product Name", "Ship Mode", COUNT(Sales)
+FROM transactions
+WHERE "Ship Mode" LIKE 'Second Class'
+GROUP BY "Product Name", "Ship Mode";
